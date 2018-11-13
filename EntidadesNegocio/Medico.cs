0a529cf23b45ef6963eac2553df0b5704327ed0b
Giancarlo <ggamarra@solucionsmart.com>
2018-11-13 16:41:28 -0500

    new commit

---
+++ EntidadesNegocio/Medico.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EntidadesNegocio
+{
+    class Medico
+    {
+        public int MyProperty { get; set; }
+    }
+}
