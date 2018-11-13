838e9940ed886aff1ba44cf81afbda56fe5dee67
spectrehck <44094066+spectrehck@users.noreply.github.com>
2018-11-13 17:08:00 -0500

    se agrego fecha de nacimiento del paciente.

--- EntidadesNegocio/Paciente.cs
+++ EntidadesNegocio/Paciente.cs
@@ -7,10 +7,12 @@ namespace EntidadesNegocio
         public string Dni { get; set; }
         public string Nombre { get; set; }
         public string Apellido { get; set; }
-        public Paciente(string dni, string nombre, string apellido) {
+        public DateTime FechaDeNacimiento { get; set; }
+        public Paciente(string dni, string nombre, string apellido, DateTime fechadenacimiento) {
             this.Dni = dni;
             this.Nombre = nombre;
             this.Apellido = apellido;
+            this.FechaDeNacimiento = fechadenacimiento;
         }
     }
 }
