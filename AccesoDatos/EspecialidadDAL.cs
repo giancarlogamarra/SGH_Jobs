c649fe4eacb8b7ab78975403edbb1dff167c03c1
Eliezer J Quispe Cabana <elie.qc95@gmail.com>
2018-11-15 12:10:14 -0500

    Creación clases Especialidad y EspecialidadDAL

---
+++ AccesoDatos/EspecialidadDAL.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Reflection;
+
+namespace AccesoDatos
+{
+    public class EspecialidadDAL
+    {
+        public static string ConexionCitas()
+        {
+            string pathBDCitas = System.IO.Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+            return pathBDCitas;
+        }
+    }
+}
---
+++ EntidadesNegocio/Especialidad.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities
+{
+    public class Especialidad
+    {
+        public string COD { get; set; }
+        public string NOM_Especialidad { get; set; }
+
+        public Especialidad (string cod, string nom_especialidad)
+        {
+            COD = cod;
+            NOM_Especialidad = nom_especialidad;
+        }
+    }
+}
