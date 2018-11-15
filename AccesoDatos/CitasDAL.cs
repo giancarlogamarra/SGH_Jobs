a4e0514a793dbd78951f03d9c63567016e907b90
Eliezer J Quispe Cabana <elie.qc95@gmail.com>
2018-11-15 11:18:15 -0500

    Creacion de AccesoDatos: DoctoresDAL, CitasDAL

---
+++ AccesoDatos/CitasDAL.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace AccesoDatos
+{
+    public class CitasDAL
+    {
+        public static string ConexionCitas()
+        {
+            string pathBDCitas = System.IO.Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+            return pathBDCitas;
+        }
+    }
+}
