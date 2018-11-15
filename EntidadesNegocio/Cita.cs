27aaa44b59fb831f4040ff9b01bed6c66d80f432
Eliezer J Quispe Cabana <elie.qc95@gmail.com>
2018-11-15 11:51:22 -0500

    Corrección clase Cita

--- EntidadesNegocio/Cita.cs
+++ EntidadesNegocio/Cita.cs
@@ -7,16 +7,18 @@ namespace Entities
     public class Cita
     {
         public string COD { get; set; }
-        public Paciente Paciente { get; set; }
-        public Doctor Doctor { get; set; }
+        public string COD_Paciente { get; set; }
+        public string COD_Especialidad { get; set; }
+        public string COD_Doctor { get; set; }
         public string Tipo { get; set; }
         public string Estado { get; set; }
 
-        public Cita(string cod, Paciente paciente, Doctor doctor, string tipo, string estado)
+        public Cita(string cod, string cod_paciente, string cod_especialidad, string cod_doctor, string tipo, string estado)
         {
             COD = cod;
-            Paciente = paciente;
-            Doctor = doctor;
+            COD_Paciente = cod_paciente;
+            COD_Especialidad = cod_especialidad;
+            COD_Doctor = cod_doctor;
             Tipo = tipo;
             Estado = estado;
         }
