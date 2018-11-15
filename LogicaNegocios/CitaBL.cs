98a42ddb2f85e0bf2e8abab7f6cbba8c5cbbc35f
Eliezer J Quispe Cabana <elie.qc95@gmail.com>
2018-11-15 11:54:17 -0500

    Creacion BusinessLayer : DoctorBL, CitaBL

---
+++ LogicaNegocios/CitaBL.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using AccesoDatos;
+using Entities;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace LogicaNegocios
+{
+    public class CitaBL
+    {
+        public static List<Cita> Listcitas;
+        public List<Cita> GetCitas()
+        {
+            string path = CitasDAL.ConexionCitas();
+            string[] lines = System.IO.File.ReadAllLines($"{path}\\BD\\Citas.txt");
+            Listcitas = new List<Cita>();
+            foreach (var item in lines)
+            {
+                string COD = item.Split(',')[0];
+                string DNI_Paciente = item.Split(',')[1];
+                string COD_Especialidad = item.Split(',')[2];
+                string COD_Doctor = item.Split(',')[3];
+                string TipoCita = item.Split(',')[4];
+                string Estado = item.Split(',')[5];
+                Cita c = new Cita(COD, DNI_Paciente, COD_Especialidad, COD_Doctor, TipoCita, Estado);
+                Listcitas.Add(c);
+            }
+            return Listcitas;
+        }
+        public void UpdateCita(string cod, Cita cita)
+        {
+            foreach (Cita item in Listcitas)
+            {
+                if (item.COD == cod)
+                {
+                    item.COD_Paciente = cita.COD_Paciente;
+                    item.COD_Especialidad = cita.COD_Especialidad;
+                    item.COD_Doctor = cita.COD_Doctor;
+                    item.Tipo = cita.Tipo;
+                    item.Estado = cita.Estado;
+                }
+            }
+            UpdateDataBase();
+        }
+        public void InsertCita(Cita cita)
+        {
+            Listcitas.Add(cita);
+            UpdateDataBase();
+        }
+        public int DeleteCita(string cod)
+        {
+            int elementsRemoved = Listcitas.RemoveAll(x => x.COD == cod);
+            UpdateDataBase();
+            return elementsRemoved;
+        }
+
+        private int UpdateDataBase()
+        {
+            string path = CitasDAL.ConexionCitas();
+            int updateLines = 0;
+            using (StreamWriter outputFile = new StreamWriter($"{path}\\BD\\Citas.txt"))
+            {
+                foreach (var item in Listcitas)
+                {
+                    string line = $"{item.COD},{item.COD_Paciente},{item.COD_Especialidad},{item.COD_Doctor},{item.Tipo},{item.Estado}";
+                    outputFile.WriteLine(line);
+                    updateLines++;
+                }
+            }
+            return updateLines;
+        }
+    }
+
+}
--- LogicaNegocios/DoctorBL.cs
+++ LogicaNegocios/DoctorBL.cs
@@ -1,10 +1,74 @@
 ﻿using System;
+using AccesoDatos;
+using Entities;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
-namespace BusinessLayer
+namespace LogicaNegocios
 {
-    class DoctorBL
+    public class DoctorBL
     {
+        public static List<Doctor> Listdoctores;
+        public List<Doctor> GetDoctores()
+        {
+            string path = DoctoresDAL.ConexionDoctores();
+            string[] lines = System.IO.File.ReadAllLines($"{path}\\BD\\Doctores.txt");
+            Listdoctores = new List<Doctor>();
+            foreach (var item in lines)
+            {
+                string Dni = item.Split(',')[0];
+                string Nombre = item.Split(',')[1];
+                string Apellido = item.Split(',')[2];
+                string CMP = item.Split(',')[3];
+                string Especialidad = item.Split(',')[4];
+                Doctor d = new Doctor(Dni, Nombre, Apellido, CMP, Especialidad);
+                Listdoctores.Add(d);
+            }
+            return Listdoctores;
+        }
+        public void UpdateDoctor(string dni, Doctor doctor)
+        {
+            foreach (Doctor item in Listdoctores)
+            {
+                if (item.DNI == dni)
+                {
+                    item.Nombre = doctor.Nombre;
+                    item.Apellido = doctor.Apellido;
+                    item.CMP = doctor.CMP;
+                    item.Especialidad = doctor.Especialidad;
+                }
+            }
+            UpdateDataBase();
+        }
+
+        public void InsertDoctor(Doctor doctor)
+        {
+            Listdoctores.Add(doctor);
+            UpdateDataBase();
+        }
+
+        public int DeleteDoctor(string Dni)
+        {
+            int elementsRemoved = Listdoctores.RemoveAll(x => x.DNI == Dni);
+            UpdateDataBase();
+            return elementsRemoved;
+        }
+
+        private int UpdateDataBase()
+        {
+            string path = DoctoresDAL.ConexionDoctores();
+            int updateLines = 0;
+            using (StreamWriter outputFile = new StreamWriter($"{path}\\BD\\Doctores.txt"))
+            {
+                foreach (var item in Listdoctores)
+                {
+                    string line = $"{item.DNI},{item.Nombre},{item.Apellido},{item.CMP},{item.Especialidad}";
+                    outputFile.WriteLine(line);
+                    updateLines++;
+                }
+            }
+            return updateLines;
+        }
     }
 }
